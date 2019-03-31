6c24137c220d50c95fbc53e83b63a7115ebf41ff
Porawit Poboonma <ball6847@gmail.com>
2019-04-01 00:24:53 +0700

    setup lerna

--- src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { YakbakModule } from './yakbak-nestjs';
-
-@Module({
-  imports: [YakbakModule],
-})
-export class AppModule {}
--- src/yakbak-nestjs/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './app';
-export * from './config';
-export * from './nestjs';
