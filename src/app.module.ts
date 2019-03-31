e1629953a67766e08253ab546e2ac10378300d26
Porawit Poboonma <ball6847@gmail.com>
2019-03-31 22:19:07 +0700

    refactor: yakbak to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { YakbakModule } from './yakbak-nestjs';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [YakbakModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 // tslint:disable:no-console
 import { NestFactory } from '@nestjs/core';
+import * as path from 'path';
 import { AppModule } from './app.module';
-import yakbak from './yakbak';
+import { createYakbakApp } from './yakbak-nestjs';
+
+// TODO: port should be configurable via command line args
+
+const yakbak = createYakbakApp({
+  tape: 'default',
+  baseDir: path.resolve('tapes'),
+});
 
 yakbak.listen(3000, () => {
-  console.log('Proxy started on localhost:3000');
+  console.log('Yakbak started on localhost:3000');
 });
 
 async function bootstrap() {
